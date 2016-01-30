1f9f1a8ef32398f10cd92496d2338a360be0beb7
Ian Qvist <qvistian@gmail.com>
2016-01-30 20:04:07 +0100

    Small naming change

--- VirusTotal.NET/UnixTimeHelper.cs
+++ VirusTotal.NET/UnixTimeHelper.cs
@@ -4,16 +4,16 @@ namespace VirusTotalNET
 {
     public static class UnixTimeHelper
     {
-        private static DateTime epoc = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        private static DateTime _epoc = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
         public static DateTime FromUnix(double unixTime)
         {
-            return epoc.AddSeconds(unixTime).ToLocalTime();
+            return _epoc.AddSeconds(unixTime).ToLocalTime();
         }
 
         public static double FromDateTime(DateTime dateTime)
         {
-            return (dateTime - epoc.ToLocalTime()).TotalSeconds;
+            return (dateTime - _epoc.ToLocalTime()).TotalSeconds;
         }
     }
 }
